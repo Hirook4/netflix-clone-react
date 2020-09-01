59c8b975b2d7908396fc741e8749cef5991c6d26
Leonardo Hirooka <leonardohirooka@hotmail.com>
2020-09-01 17:03:19 -0300

    Showing Movie List

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { useEffect, useState } from 'react';
+import './App.css';
 import Tmdb from './Tmdb';
+import MovieList from './components/MovieList';
 
 export default () => {
+
+  const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
     const loadAll = async () => {
       // Pegando a lista
       let list = await Tmdb.getHomeList();
-      console.log(list);
+      setMovieList(list);
     }
     loadAll();
 
   }, []);
   return (
-    <div>ola mundo</div>
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieList key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+    </div>
   );
 }
